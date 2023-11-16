29ec9002c23238b28e330da864e1bbf9b9374567
aaronl2026 <aaronl2026@student.cis.edu.hk>
2023-11-16 12:25:20 +0800

    new changes

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,16 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] combinedArray = new int[array1.length + array2.length];
+        int referencepoint = 0;
+        for(int i = 0; i < array1.length; i++) {
+            combinedArray[i] = array1[i];
+        }
+        for(int j = array1.length; j < array1.length + array2.length; j++){
+            combinedArray[j] = array2[referencepoint];
+            referencepoint++;
+        }
+        return combinedArray;
     }
 
     /**
@@ -21,9 +28,16 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] combinedArray = new int[array1.length + array2.length];
+        int referencepoint = 0;
+        for(int i = 0; i < array1.length; i++) {
+            combinedArray[i] = array1[i];
+        }
+        for(int j = array1.length; j < array1.length + array2.length; j++){
+            combinedArray[j] = array2[referencepoint];
+            referencepoint++;
+        }
+        return combinedArray;
     }
 
     /**
@@ -34,9 +48,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] finalarray = new int[array1.length + array2.length];
+        for(int i = 0; i < array1.length; i++){
+            finalarray[i] = array1[i] * array2[i];
+        }
+        return finalarray;
     }
 
     /**
@@ -56,14 +72,23 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
+        int[] finalarray = new int[words.length];
+        for(int i = 0; i < words.length; i++){
+            finalarray[i] = countCapitalLetters(words[i]);
+        }
+        return finalarray;
 
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
+        int finalcount = 0;
+        for(int i = 0; i < word.length(); i++){
+            int charToInt = (int) word.charAt(i);
+            if(charToInt <= 90 && charToInt >= 65){
+                finalcount = finalcount + 1;
+            }
+        }
+        return finalcount;
 
     }
 
